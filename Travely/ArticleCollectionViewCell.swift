4a519455687e3a24377199cb2a47fabdec8f0f1b
Busra Gunes <busraaguunes@gmail.com>
2022-09-29 00:50:13 +0300

    add font file

---
+++ Travely/ArticleCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  ArticleCollectionViewCell.swift
+//  Travely
+//
+//  Created by Büşra Güneş on 29.09.2022.
+//
+
+import UIKit
+
+class ArticleCollectionViewCell: UICollectionViewCell {
+    
+}
